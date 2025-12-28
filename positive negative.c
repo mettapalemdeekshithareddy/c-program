0fc80a6c9d7af3d9911f0acf10399725b197a8a1
mettapalemdeekshithareddy <mettapalemdeekshithareddy@gmail.com>
2025-12-28 23:07:43 +0530

    Add files via upload
    find if a number is positive,negative ,zero in c

---
+++ positive negative.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+int a;
+printf("enter a=");
+scanf("%d",&a);
+if(a>=0){
+printf("positive");
+}
+else if ( a<=0){
+    printf("negative");
+}
+else{
+    printf("zero");
+}
+    
+    return 0;
+}
