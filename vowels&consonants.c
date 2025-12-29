73baa72b497ab14b7284428af5400d857dba2598
mettapalemdeekshithareddy <mettapalemdeekshithareddy@gmail.com>
2025-12-29 23:05:52 +0530

    Add files via upload

---
+++ vowels&consonants.c
@@ -0,0 +1,18 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    char ch;
+    printf("enter ch=");
+    scanf("%c",&ch);
+    if((ch>='a' && ch<='z') ||(ch>='A'&& ch<='Z'))
+    if( ch=='a' || ch=='e' || ch=='i' ||ch=='o' ||  ch=='u' ||
+    ch=='A' || ch=='E' ||ch=='I' ||ch=='O'|| ch=='U' ) {
+        printf("vowels");
+    }
+    else{
+        printf("consonants");
+    }
+
+    return 0;
+}
