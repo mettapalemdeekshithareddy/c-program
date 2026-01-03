c4a4fc70ea434c2bbfa1d8856bbbf8ce16021829
mettapalemdeekshithareddy <mettapalemdeekshithareddy@gmail.com>
2026-01-03 21:46:20 +0530

    Add files via upload
    write a c program to find sum of array elements

---
+++ array(sum).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a[]={1, 2, 3 ,4};
+    int sum=0;
+    for( int i = 0; i < 4; i++){
+        sum=sum+a[i];
+        printf("%d",sum);
+    
+    }
+
+    return 0;
+}
