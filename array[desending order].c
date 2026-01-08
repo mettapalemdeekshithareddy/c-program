c7697c3af8bb8dc78d3957d8cc3add6bb61f612a
mettapalemdeekshithareddy <mettapalemdeekshithareddy@gmail.com>
2026-01-08 23:13:46 +0530

    Add files via upload

---
+++ array[desending order].c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, temp;
+    int a[100];
+
+    printf("Enter n : ");
+    scanf("%d", &n);
+
+    printf("\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    for (i = 0; i < n - 1; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (a[i] < a[j]) {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+
+    printf("Descending order:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
