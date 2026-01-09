656ea485581bf0a5a98e0394746ca14a8ff892da
mettapalemdeekshithareddy <mettapalemdeekshithareddy@gmail.com>
2026-01-09 23:06:31 +0530

    Add files via upload
    write a  c program to count vowels in a string

---
+++ string[vowels].c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    char s[20];
+    int i, count = 0;
+
+    printf("Enter string: ");
+    scanf("%s", s);
+
+    for (i = 0; s[i] != '\0'; i++) {
+        if (s[i]=='a' || s[i]=='e' || s[i]=='i' ||
+            s[i]=='o' || s[i]=='u' ||
+            s[i]=='A' || s[i]=='E' || s[i]=='I' ||
+            s[i]=='O' || s[i]=='U') {
+            count++;
+        }
+    }
+
+    printf(" %d", count);
+    return 0;
+}
