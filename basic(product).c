68927c846ca04715639c706bebfc37a74c21a0a3
mettapalemdeekshithareddy <mettapalemdeekshithareddy@gmail.com>
2026-02-09 22:46:12 +0530

    Add files via upload

---
+++ basic(product).c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a,b,product;
+    printf("enter a:");
+    scanf("%d",&a);
+    printf("enter b:");
+    scanf("%d",&b);
+    product=a*b;
+    printf("%d",product);
+    return 0;
+}
