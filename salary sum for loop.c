3ec66e5128daba16e08487378de4b3a359107c67
mettapalemdeekshithareddy <mettapalemdeekshithareddy@gmail.com>
2026-02-19 21:27:25 +0530

    Add files via upload

---
+++ salary sum for loop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    int day;
+    scanf("%d",&day);
+    int salary;
+    for(int i=0;i<n;i++){
+         salary=n-(day*100);
+        
+    }printf("%d",salary);
+
+    
+    return 0;
+}
